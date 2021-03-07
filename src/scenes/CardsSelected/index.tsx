2048bf66d84348ac23d2c969f8ee3f40529854b2
Dimitris Tsiktsiris <dtsiktsiris@outlook.com>
2021-03-07 14:51:33 +0200

    add selected cards component

--- src/scenes/CardsSelected/index.tsx
+++ src/scenes/CardsSelected/index.tsx
@@ -2,16 +2,17 @@ import React, { useState } from 'react'
 import './index.css'
 import Card from '../../components/Card/';
 
-function CardsHolder(){
+function CardsSelected(){
+    const [selected, selectedClicked] = useState('');
     
-    const numbers = ['1', '2', '3', '5','8','13','20','40','60','100', '?','C'];
+    const numbers = ['1', '2', '3', '5','8'];
     const listItems = numbers.map((number) => {
         return <Card key={number} number={number}></Card>  
     });
     
     return(
         <div className="holder">
-            <p>Choose you estimate:</p>
+            <p>3 users have selected:</p>
             <div className="cards">
                 {listItems}
             </div>
@@ -19,4 +20,4 @@ function CardsHolder(){
     )
 }
 
-export default CardsHolder;
+export default CardsSelected;
